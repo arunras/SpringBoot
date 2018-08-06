9115291c2caf901de1b3d576d2bee12e35546733
Arun <prith000@citymail.cuny.edu>
2018-08-06 14:44:12 -0500

    add convert map to list

--- CoreJava/src/main/java/core/java/collections/ListToMap.java
+++ CoreJava/src/main/java/core/java/collections/ListToMap.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import core.java.pojo.Student;
+
 public class ListToMap {
 	
 	public void convertListToMap () {
--- CoreJava/src/main/java/core/java/pojo/Student.java
+++ CoreJava/src/main/java/core/java/pojo/Student.java
@@ -1,4 +1,4 @@
-package core.java.collections;
+package core.java.pojo;
 
 public class Student {
 	private int id;
